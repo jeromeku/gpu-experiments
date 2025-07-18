3692606002283166233e969cf08a3500d2f47977
Stuart Sul <ssstuartss@gmail.com>
2025-07-18 21:56:57 +0000

    Add int32 load/store tests

--- blackwell/04-tm-ldst-1d-int32.cu
+++ blackwell/04-tm-ldst-1d-int32.cu
@@ -1,3 +1,14 @@
+/*
+    Tensor memory simple load/store
+
+    Observations:
+        - Each warp can only access 32 lanes of the TM
+        - Thus, whatever TM address we pass, the lane value is ** AUTOMATICALLY mod-32'ed ** and added warp_id * 32.
+        - For instance, if I pass in 0x0060'0000 to warp 0 (which refer to lane 96), it will just mod the lane and access lane 0
+        - Likely, if I pass in lane 0 to warp 3, it will access lane 96
+*/
+
+
 #include "kittens.cuh"
 #include "prototype.cuh"
 #include "pyutils/pyutils.cuh"
@@ -5,14 +16,11 @@
 using namespace kittens;
 using namespace kittens::prototype;
 
-// ThunderKittens macro check
-#if !defined(KITTENS_HOPPER) || !defined(KITTENS_BLACKWELL)
-    #error "KITTENS_HOPPER and KITTENS_BLACKWELL macros must be defined for Blackwell compilation"
-#endif
-
 // Kernel globals
 struct globals {
-    gl<int, 1, -1, -1, -1> tensor;
+    // constexpr int BLOCK_SIZE = 128;
+
+    gl<int, 1, 1, 1, -1> tensor;
 
     __host__ inline dim3 grid() { return dim3(1); } // use single block
     __host__ inline dim3 block() { return dim3(128); } // use single warpgroup
@@ -38,15 +46,15 @@ __global__ void kernel(const __grid_constant__ globals G) {
     // TM store launched by threads 0, ..., 127
     int src = threadIdx.x;
     asm volatile("tcgen05.st.sync.aligned.16x64b.x1.b32 [%0], {%1};"
-        :: "r"(tm_addr), "r"(src)
+        :: "r"(tm_addr + (3 - (threadIdx.x / 32)) * 0x0020'0000), "r"(src)
     );
     asm volatile("tcgen05.wait::st.sync.aligned;"); // waits for st issued by current thread
     asm volatile("bar.sync %0, %1;" :: "n"(0), "n"(128)); // warpgroup sync
-    
+
     // TM load launched by threads 0, ..., 127
     int dst = 0; // to truly check if value got loaded
     asm volatile("tcgen05.ld.sync.aligned.16x64b.x1.b32 {%1}, [%0];"
-        :: "r"(tm_addr), "r"(dst)
+        :: "r"(tm_addr + (threadIdx.x / 32) * 0x0020'0000), "r"(dst) // although different addr, mod 32 will make TM access per warp the same
     );
     asm volatile("tcgen05.wait::st.sync.aligned;"); // waits for st issued by current thread
     asm volatile("bar.sync %0, %1;" :: "n"(0), "n"(128)); // warpgroup sync
--- blackwell/04-tm-ldst-1d-int32.py
+++ blackwell/04-tm-ldst-1d-int32.py
@@ -1,8 +1,8 @@
 # Game plans (implementation order)
 # 
-# 1. (Python) Create an empty global tensor of 128x128 + float8
-# 2. Fill in the first 128x128 section of TM with values 0, 1, 2, .. (row major), no packing
-# 3. Load from the first 128x128 section of TM into registers
+# 1. (Python) Create an empty global tensor of 128 + int32
+# 2. Fill in the first 128 section of TM with values 0, 1, 2, .. (row major), no packing
+# 3. Load from the first 128 section of TM into registers
 # 4. Naively save the register values to the empty global tensor
 # 5. (Python) Check that empty global tensor has values 0, 1, 2, ...
 #
@@ -13,10 +13,9 @@ import torch
 
 from _C import kernel
 
-tensor = torch.zeros(128, 128, dtype=torch.int32, device="cuda")
+t = torch.zeros(128, dtype=torch.int32, device="cuda")
 
-kernel(tensor)
+kernel(t)
 torch.cuda.synchronize()
 
-print(tensor)
-breakpoint()
+print(t)
