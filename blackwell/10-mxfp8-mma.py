a4b3e38386a56229c697f92324d3f15d000129ad
Stuart Sul <ssstuartss@gmail.com>
2025-07-20 02:33:45 +0000

    Add benchmark

--- blackwell/10-mxfp8-mma.py
+++ blackwell/10-mxfp8-mma.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 torch.random.manual_seed(42)
 
@@ -132,3 +133,33 @@ mean_ref = C_ref.mean().item()
 print(f"Max adiff: {abs_diff.max()}")
 print(f"Mean adiff: {abs_diff.mean()}")
 print("Mean diff:", bias, "Mean ref:", mean_ref)
+
+# Benchmark
+NUM_WARMUPS = 5
+NUM_ITERS = 10
+
+start_events = [torch.cuda.Event(enable_timing=True) for _ in range(NUM_ITERS)]
+end_events = [torch.cuda.Event(enable_timing=True) for _ in range(NUM_ITERS)]
+
+for i in range(NUM_WARMUPS):
+    kernel(A_fp8, A_sc, B_fp8, B_sc, C)
+
+l2_cache_size = 1024 * 1024 * 50 # 50 MB for Hopper
+l2_cache = torch.randn(l2_cache_size // 2, dtype=torch.bfloat16)
+cache_clear = lambda: l2_cache.random_(0, 1)
+
+for i in range(NUM_ITERS):
+    cache_clear()
+    start_events[i].record()
+    kernel(A_fp8, A_sc, B_fp8, B_sc, C)
+    end_events[i].record()
+torch.cuda.synchronize()
+
+times = [s.elapsed_time(e) for s, e in zip(start_events, end_events)]
+avg_time = np.mean(times) * 1e-3
+std_time = np.std(times) * 1e-3
+flops = 2.0 * M * N * K
+tflops = flops * 1e-12
+
+print(f"Average time: {avg_time * 1e6:.2f} ± {std_time * 1e6:.2f} us")
+print(f"Average TFLOPS: {tflops / (avg_time):.2f} TFLOp/s")
