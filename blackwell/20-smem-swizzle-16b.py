593f49ac51b46124874142b9a06ee3f9bae5905a
Stuart Sul <ssstuartss@gmail.com>
2025-07-23 20:20:44 +0000

    Fix 16 swizzle test

--- blackwell/20-smem-swizzle-16b.py
+++ blackwell/20-smem-swizzle-16b.py
@@ -13,7 +13,7 @@ N = 128
 
 # Globals
 print("Initializing globals...")
-inputs = torch.arange(N, dtype=torch.bfloat16, device="cuda").unsqueeze(0).repeat_interleave(M, dim=0)
+inputs = torch.arange(M * N, dtype=torch.int32, device="cuda").reshape(M, N).to(torch.uint16).view(torch.bfloat16)
 loaded = torch.zeros(M, N, dtype=torch.bfloat16, device="cuda")
 stored = torch.zeros(M, N, dtype=torch.bfloat16, device="cuda")
 
@@ -23,15 +23,15 @@ kernel(inputs, loaded, stored)
 torch.cuda.synchronize()
 
 # Convert back for comparison
-loaded = loaded.to(torch.float32)
-stored = stored.to(torch.float32)
+loaded = loaded.view(torch.uint16).to(torch.int32)
+stored = stored.view(torch.uint16).to(torch.int32)
 
 print(loaded)
 print(stored)
 
 abs_diff = (loaded - stored).abs()
 print(f"Max adiff: {abs_diff.max().item()}")
-print(f"Mean adiff: {abs_diff.mean().item()}")
+print(f"Mean adiff: {abs_diff.to(torch.float32).mean().item()}")
 
 row_sum = loaded.sum(dim=1)
 print(f"Row sum: {row_sum}")
@@ -40,8 +40,13 @@ col_sum = loaded.sum(dim=0)
 print(f"Col sum: {col_sum}")
 
 for i in range(128 // 16):
-    abs_diff = (loaded[:16] - loaded[i * 16:(i + 1) * 16]).abs()
-    print(f"Max adiff: {abs_diff.max().item()}")
+    # This doesn't work. Ignore
+    if i < 8:
+        abs_diff = (loaded[:16] - loaded[i * 16:(i + 1) * 16]).abs()
+        print(f"Max adiff: {abs_diff.max().item()}")
+    else:
+        abs_diff = (loaded[8 * 16:128] - loaded[i * 16:(i + 1) * 16]).abs()
+        print(f"Max adiff: {abs_diff.max().item()}")
 
 if True:
     np.savetxt('loaded_matrix.txt', loaded.cpu().numpy(), fmt='%3d', delimiter=' ')
