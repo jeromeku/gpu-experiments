7818d7f0b0db75621dcd1f6d0c63623f8993e4b2
Stuart Sul <ssstuartss@gmail.com>
2025-09-03 07:19:10 +0000

    Add fully working all2all implementation -- slow though

---
+++ blackwell/60-all2all.cu
@@ -0,0 +1,167 @@
+/*
+    Goal: implement fast single-node all2all collective communication
+*/
+
+#include <torch/extension.h>
+
+#include "kittens.cuh"
+#include "prototype.cuh"
+#include "pyutils/pyutils.cuh"
+#include "pyutils/torchutils.cuh"
+
+using namespace kittens;
+using namespace kittens::prototype;
+
+struct config {
+    static constexpr int CLUSTER_SIZE = 1;
+    static constexpr int NUM_BLOCKS = 148; // TODO: vary this
+    static constexpr int STATIC_SHARED_MEMORY = 128;
+    static constexpr int DYNAMIC_SHARED_MEMORY = MAX_SHARED_MEMORY - STATIC_SHARED_MEMORY;
+
+    static constexpr int NUM_WARPS = 2;
+    static constexpr int NUM_THREADS = NUM_WARPS * WARP_THREADS;
+};
+
+struct globals {
+    static constexpr int NUM_DEVICES = 8;
+    static constexpr int ROW_BLOCK_SIZE = 16;
+    static constexpr int COL_BLOCK_SIZE = 128;
+    static constexpr int PIPELINE_STAGES = 16;
+
+    using tile = st_bf<ROW_BLOCK_SIZE, COL_BLOCK_SIZE>;
+    using parallel_layout = pgl<gl<bf16, -1, -1, -1, -1, tma::descriptor<tile, 2, false>>, NUM_DEVICES, false>;
+
+    parallel_layout src;
+    parallel_layout dst;
+    const int dev_idx;
+};
+
+__device__ inline int4 get_indices(const globals &G, int task_id) {
+    int4 indices;
+
+    indices.x = task_id / (G.src.depth() * (G.src.rows() / globals::ROW_BLOCK_SIZE) * (G.src.cols() / globals::COL_BLOCK_SIZE));
+    task_id -= indices.x * (G.src.depth() * (G.src.rows() / globals::ROW_BLOCK_SIZE) * (G.src.cols() / globals::COL_BLOCK_SIZE));
+    indices.y = task_id / ((G.src.rows() / globals::ROW_BLOCK_SIZE) * (G.src.cols() / globals::COL_BLOCK_SIZE));
+    task_id -= indices.y * ((G.src.rows() / globals::ROW_BLOCK_SIZE) * (G.src.cols() / globals::COL_BLOCK_SIZE));
+    indices.z = task_id / (G.src.cols() / globals::COL_BLOCK_SIZE);
+    task_id -= indices.z * (G.src.cols() / globals::COL_BLOCK_SIZE);
+    indices.w = task_id;
+
+    if (indices.x >= G.src.batch())
+        return { -1, -1, -1, -1 };
+    else
+        return indices;
+}
+
+template <int SCATTER_AXIS = 2, int GATHER_AXIS = 1>
+__device__ inline void kernel(const globals &G) {
+    // Shared memory declaration
+    extern __shared__ int __shm[];
+    tma_swizzle_allocator allocator((int*)&__shm[0]);
+
+    // Warp configuration
+    const int warp_id = kittens::warpid();
+    const int lane_id = warp::laneid();
+
+    // Allocate shared memory
+    static_assert(sizeof(globals::tile) * globals::PIPELINE_STAGES <= config::DYNAMIC_SHARED_MEMORY);
+    globals::tile (&tiles)[globals::PIPELINE_STAGES] = allocator.allocate<globals::tile, globals::PIPELINE_STAGES>();
+
+    // Set up mbarriers
+    __shared__ semaphore inputs_arrived[globals::PIPELINE_STAGES];
+    __shared__ semaphore inputs_finished[globals::PIPELINE_STAGES];
+    if (threadIdx.x == 0) {
+        for (int i = 0; i < globals::PIPELINE_STAGES; ++i) {
+            init_semaphore(inputs_arrived[i], 0, 1);
+            init_semaphore(inputs_finished[i], 0, 1);
+        }
+    }
+    __syncthreads();
+
+    // Declare stage and phasebits for semaphore waits
+    int stage = 0;
+    uint32_t phasebits = 0xFFFF0000;
+
+    // Main divergence
+    if (warp_id == 0 && lane_id < globals::PIPELINE_STAGES) {
+        for (int task_id = blockIdx.x; true; task_id += gridDim.x) {
+            int4 indices = get_indices(G, task_id);
+            if (indices.x == -1) break;
+
+            if (lane_id == stage) {
+                wait(inputs_finished[stage], get_phasebit<1>(phasebits, stage));
+                update_phasebit<1>(phasebits, stage);
+    
+                tma::expect_bytes(inputs_arrived[stage], sizeof(globals::tile));
+                asm volatile("{cp.async.bulk.tensor.4d.shared::cta.global.tile.mbarrier::complete_tx::bytes.cta_group::1 [%0], [%1, {%2, %3, %4, %5}], [%6];}"
+                    :: "r"(static_cast<uint32_t>(__cvta_generic_to_shared(&tiles[stage][0]))), 
+                       "l"(G.src[G.dev_idx].template get_tma<globals::tile, 2>()), 
+                       "r"(indices.w * globals::COL_BLOCK_SIZE), "r"(indices.z * globals::ROW_BLOCK_SIZE), "r"(indices.y), "r"(indices.x),
+                       "r"(static_cast<uint32_t>(__cvta_generic_to_shared(&inputs_arrived[stage])))
+                    : "memory");
+            }
+
+            stage = (stage + 1) % globals::PIPELINE_STAGES;
+        }
+    } else if (warp_id == 1 && lane_id < globals::PIPELINE_STAGES) {
+        // Scatter/gather configuration
+        // TODO: branch based on actual axis
+        const int scatter_blocks_per_dev = G.src.rows() / globals::ROW_BLOCK_SIZE / globals::NUM_DEVICES;
+        const int gather_blocks_per_dev = G.dst.depth() / globals::NUM_DEVICES; // important to use dst not src
+
+        for (int task_id = blockIdx.x; true; task_id += gridDim.x) {
+            int4 indices = get_indices(G, task_id);
+            if (indices.x == -1) break;
+
+            // Decide which device to send to with scatter axis
+            int dst_dev_idx = reinterpret_cast<int *>(&indices)[SCATTER_AXIS] / scatter_blocks_per_dev;
+
+            // Decide which location in the scatter axis to send to with destination device index
+            int scatter_axis_base = scatter_blocks_per_dev * dst_dev_idx; // will be subtracted
+
+            // Decide which location in the gather axis to send to with current device index
+            int gather_axis_base = gather_blocks_per_dev * G.dev_idx; // will be added
+
+            if (lane_id == stage) {
+                wait(inputs_arrived[stage], get_phasebit<0>(phasebits, stage));
+                update_phasebit<0>(phasebits, stage);
+
+                asm volatile("{fence.proxy.async.shared::cta;}" ::: "memory"); // make writes to smem visible
+                asm volatile("{cp.async.bulk.tensor.4d.global.shared::cta.tile.bulk_group [%0, {%1, %2, %3, %4}], [%5];}"
+                    :: "l"(G.dst[dst_dev_idx].template get_tma<globals::tile, 2>()),
+                       "r"(indices.w * globals::COL_BLOCK_SIZE), "r"((indices.z - scatter_axis_base) * globals::ROW_BLOCK_SIZE), "r"(indices.y + gather_axis_base), "r"(indices.x), // TODO
+                       "r"(static_cast<uint32_t>(__cvta_generic_to_shared(&tiles[stage][0])))
+                    : "memory");
+                asm volatile("{cp.async.bulk.commit_group;}");
+                asm volatile("{cp.async.bulk.wait_group.read %0;}" :: "n"(0) : "memory");
+
+                arrive(inputs_finished[stage]);
+            }
+
+            stage = (stage + 1) % globals::PIPELINE_STAGES;
+        }
+    }
+}
+
+void entrypoint(const at::Tensor &dst, const at::Tensor &src, KittensBroker &broker) {
+    kittens::py::device_check(dst, src);
+
+    KittensBond src_bonds[globals::NUM_DEVICES];
+    KittensBond dst_bonds[globals::NUM_DEVICES];
+
+    // Instantiate globals
+    globals G {
+        .src = kittens::py::tensor_to_pgl<typename globals::parallel_layout>(src, broker, src_bonds),
+        .dst = kittens::py::tensor_to_pgl<typename globals::parallel_layout>(dst, broker, dst_bonds),
+        .dev_idx = broker.local_rank_
+    };
+
+    // Run kernel
+    kittens::py::launch_kernel<config, globals, kernel<2, 1>>(G);
+}
+
+PYBIND11_MODULE(_C, m) {
+    pybind11::class_<KittensBroker>(m, "KittensBroker")
+        .def(pybind11::init<int, int>());
+    m.def("all2all", &entrypoint);
+}
--- blackwell/60-all2all.py
+++ blackwell/60-all2all.py
@@ -4,7 +4,6 @@ torchrun --nproc_per_node=8 60-all2all.py
 
 from datetime import timedelta
 import os
-import sys
 import time
 
 import numpy as np
@@ -12,64 +11,67 @@ import torch
 import torch.distributed
 torch.set_printoptions(sci_mode=False)
 
+from _C import KittensBroker, all2all
 
-def all2all_4D(
-    input: torch.tensor,
+
+def check_diff(name, A, A_ref):
+    print(f"\n{name}")
+    print(f"Max diff:  {((A - A_ref).abs().max().item()):.10f}")
+    print(f"Mean diff: {((A - A_ref).abs().mean().item()):.10f}")
+    print(f"Mean:      {A.abs().mean().item():.10f}")
+    print(f"Ref mean:  {A_ref.abs().mean().item():.10f}")
+    print(f"Max:       {A.abs().max().item():.10f}")
+    print(f"Ref max:   {A_ref.abs().max().item():.10f}")
+
+
+def all2all_ref(
+    src: torch.Tensor,
     scatter_idx: int = 2,
-    gather_idx: int = 1,
-    group: torch.distributed.ProcessGroup | None = None,
-    sync: bool = False
-) -> torch.tensor:
+    gather_idx: int = 1
+) -> torch.Tensor:
     """
     all-to-all collective operation on a 4D tensor. Gathers on gather_idx, scatters on scatter_idx.
 
     Args:
-        input: torch.tensor sharded on gather_idx
+        src: torch.tensor sharded on gather_idx
         scatter_idx: index to scatter
         gather_idx: index to gather
-        group: torch process group
-        sync: whether to synchronize after all-to-all
 
     Returns:
         torch.tensor: torch.tensor gathered on gather_idx and sharded on scatter_idx
     """
-    assert input.dim() == 4, f"input must be 4D tensor, got {input.dim()} and shape {input.shape}"
 
-    num_ranks = torch.distributed.get_world_size(group)
+    num_ranks = torch.distributed.get_world_size()
 
     if scatter_idx == 2 and gather_idx == 1:
-        B, N_per_rank, H, D = input.shape
+        B, N_per_rank, H, D = src.shape
         N = N_per_rank * num_ranks
         H_per_rank = H // num_ranks
 
-        input_t = input.view(B, N_per_rank, num_ranks, H_per_rank, D).permute(2, 1, 0, 3, 4).contiguous()
+        src_t = src.view(B, N_per_rank, num_ranks, H_per_rank, D).permute(2, 1, 0, 3, 4).contiguous()
         
         if num_ranks > 1:
-            output = torch.empty_like(input_t)
-            torch.distributed.all_to_all_single(output, input_t, group=group)
-            if sync:
-                torch.cuda.synchronize()
+            dst = torch.empty_like(src_t)
+            torch.distributed.all_to_all_single(dst, src_t)
         else:
-            output = input_t
+            dst = src_t
 
-        return output.permute(2, 0, 1, 3, 4).reshape(B, N_per_rank * num_ranks, H_per_rank, D)
+        return dst.permute(2, 0, 1, 3, 4).reshape(B, N_per_rank * num_ranks, H_per_rank, D)
     
     elif scatter_idx == 1 and gather_idx == 2:
-        B, N, H_per_rank, D = input.shape
+        B, N, H_per_rank, D = src.shape
         H = H_per_rank * num_ranks
         N_per_rank = N // num_ranks
 
-        input_t = input.view(B, num_ranks, N_per_rank, H_per_rank, D).permute(1, 3, 2, 0, 4).contiguous()
+        src_t = src.view(B, num_ranks, N_per_rank, H_per_rank, D).permute(1, 3, 2, 0, 4).contiguous()
         
         if num_ranks > 1:
-            output = torch.empty_like(input_t)
-            torch.distributed.all_to_all_single(output, input_t, group=group)
-            if sync:
-                torch.cuda.synchronize()
+            dst = torch.empty_like(src_t)
+            torch.distributed.all_to_all_single(dst, src_t)
         else:
-            output = input_t
+            dst = src_t
 
-        return output.permute(3, 2, 0, 1, 4).reshape(B, N_per_rank, H_per_rank * num_ranks, D)
+        return dst.permute(3, 2, 0, 1, 4).reshape(B, N_per_rank, H_per_rank * num_ranks, D)
     
     else:
         raise RuntimeError("scatter_idx must be 1 or 2 and gather_idx must be 1 or 2")
@@ -78,8 +80,7 @@ def all2all_4D(
 # Parameters
 N = 65536
 H = 128
-D_qk = 192
-D_vo = 128
+D = 128
 
 # Initialize distributed environment
 rank = int(os.environ.get("RANK", 0))
@@ -97,48 +98,98 @@ torch.distributed.init_process_group(
     timeout=timedelta(seconds=30),
 )
 
+# Initialize KittensBroker
+broker = KittensBroker(local_rank, local_world_size)
+
 # Print global parameters
 if rank == 0:
-    print(f"N: {N}, H: {H}, D_qk: {D_qk}, D_vo: {D_vo}")
+    print(f"N: {N}, H: {H}, D: {D}")
 
 # Generate inputs
 torch.random.manual_seed(rank + 42)
-A = torch.randn(1, N // world_size, H, D_qk, dtype=torch.bfloat16, device=device)
+src = torch.randn(1, N // world_size, H, D, dtype=torch.bfloat16, device=device)
+dst = torch.zeros(1, N, H // world_size, D, dtype=torch.bfloat16, device=device)
 
 # Run PyTorch reference
-all2all_4D(A, scatter_idx=2, gather_idx=1)
+dst_ref = all2all_ref(src, scatter_idx=2, gather_idx=1)
+torch.distributed.barrier()
 torch.cuda.synchronize()
+
+# Run kernel
+all2all(dst, src, broker)
+torch.distributed.barrier()
+torch.cuda.synchronize()
+
+# Check correctness
+for i in range(world_size):
+    if i == rank:
+        check_diff(f"Rank {rank}", dst, dst_ref)
+    torch.distributed.barrier()
 
 # Benchmark
 NUM_WARMUPS = 5
 NUM_ITERS = 10
-chunk_size = (N // world_size) * (H // world_size) * D_qk * 2 # chunk sent from one rank to another
+chunk_size = (N // world_size) * (H // world_size) * D * 2 # chunk sent from one rank to another
 per_rank_comm_size = chunk_size * (world_size - 1)
 total_comm_size = per_rank_comm_size * world_size # N * (N - 1) * chunk_size
 l2_cache = torch.empty(1024 * 1024 * 64, dtype=torch.bfloat16, device=device)
 
 for i in range(NUM_WARMUPS):
-    all2all_4D(A, scatter_idx=2, gather_idx=1)
+    all2all_ref(src, scatter_idx=2, gather_idx=1)
 torch.distributed.barrier()
 torch.cuda.synchronize()
 
 times = []
+start_event = torch.cuda.Event(enable_timing=True)
+end_event = torch.cuda.Event(enable_timing=True)
 
 for i in range(NUM_ITERS):
     l2_cache.random_()
     torch.distributed.barrier()
     torch.cuda.synchronize()
-    t0 = time.perf_counter()
-    all2all_4D(A, scatter_idx=2, gather_idx=1)
+    start_event.record()
+    all2all_ref(src, scatter_idx=2, gather_idx=1)
+    end_event.record()
     torch.cuda.synchronize()
-    t1 = time.perf_counter()
-    times.append(t1 - t0)
+    times.append(start_event.elapsed_time(end_event) * 1e-3)
 
 avg_time = np.mean(times)
 std_time = np.std(times)
 
 if rank == 0:
-    print("B200 Max Unidirectional NVL Bandwidth: 900 GB/s")
+    print("\nB200 Max Unidirectional NVL Bandwidth: 900 GB/s")
+    print(f"Per-rank data sent: {per_rank_comm_size * 1e-9:.2f} GB")
+
+for i in range(world_size):
+    if i == rank:
+        print(f'Time taken: {avg_time * 1e6:.2f} ± {std_time * 1e6:.2f} us')
+        print(f'Unidirectional Bandwidth, USP: {per_rank_comm_size * 1e-9 / avg_time:.2f} GB/s')
+    torch.distributed.barrier()
+
+for i in range(NUM_WARMUPS):
+    all2all(dst, src, broker)
+torch.distributed.barrier()
+torch.cuda.synchronize()
+
+times = []
+start_event = torch.cuda.Event(enable_timing=True)
+end_event = torch.cuda.Event(enable_timing=True)
+
+for i in range(NUM_ITERS):
+    l2_cache.random_()
+    torch.distributed.barrier()
+    torch.cuda.synchronize()
+    start_event.record()
+    all2all(dst, src, broker)
+    end_event.record()
+    torch.cuda.synchronize()
+    times.append(start_event.elapsed_time(end_event) * 1e-3)
+
+avg_time = np.mean(times)
+std_time = np.std(times)
+
+if rank == 0:
+    print("\nB200 Max Unidirectional NVL Bandwidth: 900 GB/s")
     print(f"Per-rank data sent: {per_rank_comm_size * 1e-9:.2f} GB")
 
 for i in range(world_size):
