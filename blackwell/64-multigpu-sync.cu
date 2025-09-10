45bc20adfbf72ae52673a2d7248ee5bd7450320f
Stuart Sul <ssstuartss@gmail.com>
2025-09-10 02:10:57 +0000

    Add multigpu sync barrier example

---
+++ blackwell/64-multigpu-sync.cu
@@ -0,0 +1,46 @@
+#include "kittens.cuh"
+#include "prototype.cuh"
+#include "pyutils/torchutils.cuh"
+
+using namespace kittens;
+using namespace kittens::prototype;
+
+struct config {
+    static constexpr int CLUSTER_SIZE = 1;
+    
+    static constexpr int NUM_BLOCKS = 148;
+
+    static constexpr int NUM_WARPGROUPS = 3;
+    static constexpr int NUM_WARPS = NUM_WARPGROUPS * WARPGROUP_WARPS;
+    static constexpr int NUM_THREADS = NUM_WARPS * WARP_THREADS;
+
+    static constexpr int DYNAMIC_SHARED_MEMORY = 0;
+};
+
+struct globals {
+    static constexpr int NUM_DEVICES = 8;
+    device<NUM_DEVICES>::barrier_t barriers;
+    const int dev_idx;
+};
+
+__device__ inline void kernel(const globals &G) {
+    device<globals::NUM_DEVICES>::sync(G.barriers, G.dev_idx);
+}
+
+void entrypoint(kittens::py::TKParallelTensor &barriers) {
+    // Instantiate globals
+    globals G {
+        .barriers = kittens::py::parallel_tensor_to_pgl<device<globals::NUM_DEVICES>::barrier_t>(barriers),
+        .dev_idx = barriers.local_rank_
+    };
+
+    // Run kernel
+    kittens::py::launch_kernel<config, globals, kernel>(G);
+}
+
+#include <torch/csrc/utils/pybind.h>
+
+PYBIND11_MODULE(_C, m) {
+    BIND_TK_PARALLEL_TENSOR(m);
+    m.def("sync", &entrypoint);
+}
---
+++ blackwell/64-multigpu-sync.py
@@ -0,0 +1,61 @@
+"""
+torchrun --nproc_per_node=8 61-vmm-ipc.py
+"""
+
+from datetime import timedelta
+import os
+from time import sleep
+
+import torch
+import torch.distributed
+torch.set_printoptions(sci_mode=False)
+
+from _C import TKParallelTensor, sync
+
+
+# Initialize distributed environment
+rank = int(os.environ.get("RANK", 0))
+local_rank = int(os.environ.get("LOCAL_RANK", 0))
+local_world_size = int(os.environ.get("LOCAL_WORLD_SIZE", 1))
+world_size = int(os.environ.get("WORLD_SIZE", 1))
+assert world_size == local_world_size, "multi-node runs are not supported"
+device = torch.device(f"cuda:{local_rank}")
+torch.cuda.set_device(device)
+torch.distributed.init_process_group(
+    backend="nccl",
+    device_id=local_rank,
+    rank=rank,
+    world_size=world_size,
+    timeout=timedelta(seconds=30),
+)
+
+# Allocate tensor
+barriers = TKParallelTensor((1,), dtype=torch.int, local_rank=local_rank, local_world_size=local_world_size, multicast=True)
+
+# Run our function
+if False:
+    print("Profiling...")
+    with torch.profiler.profile(
+        activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+        record_shapes=True,
+        profile_memory=True,
+        with_modules=True,
+        with_stack=True
+    ) as profiler:
+        sleep(local_rank)
+        sync(barriers)
+
+    # Export to Chrome trace format
+    if rank == 0:
+        profiler.export_chrome_trace(f"trace.json")
+        print(f"\nProfiler trace exported to trace.json")
+
+else:
+    sleep(local_rank)
+    print(f"Rank {local_rank} started")
+    sync(barriers)
+    torch.cuda.synchronize()
+    print(f"Rank {local_rank} done")
+
+# Clean up
+torch.distributed.destroy_process_group()
